df60a4e6ae18e0485778dbc2f33a2f43a4a3fea6
Nilesh Pimple <nilesh.pimple@cognizant.com>
2021-08-07 15:15:23 +0530

    Initial commiit

---
+++ ContactsManager.Domain/Entities/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager.Domain.Entities
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
